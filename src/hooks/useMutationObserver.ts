d904c431604ceaa81c82cc01eea6aaeccc7b7b0e
bluewarn <n5lp97@gmail.com>
2022-10-04 18:37:14 +0900

    Add useMutationObserver

---
+++ src/hooks/useMutationObserver.ts
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function useMutationObserver(
+    ref: React.RefObject<HTMLElement>,
+    callback: (mutationRecord: MutationRecord[]) => void,
+    options = {
+        attributes : true,
+        attributeFilter : ['class']
+    }
+) {
+    React.useEffect(() => {
+        if (ref.current) {
+            const observer = new MutationObserver(callback);
+            observer.observe(ref.current, options);
+            
+            return () => observer.disconnect();
+        }
+    }, [callback, options]);
+}
