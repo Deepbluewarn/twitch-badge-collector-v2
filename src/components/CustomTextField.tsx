03ff884f2a1ed2a00002ca2e7250cbb8275579bf
bluewarn <n5lp97@gmail.com>
2022-12-24 13:56:31 +0900

    Add: 컴포넌트 추가

---
+++ src/components/CustomTextField.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import {
+  Context as TBCContext,
+  SettingInterface,
+} from 'twitch-badge-collector-cc';
+import TextField from "@mui/material/TextField";
+
+export default function CustomTextField(props: {
+  title: string;
+  id: string;
+}) {
+  const { globalSetting, dispatchGlobalSetting } = TBCContext.useGlobalSettingContext();
+
+  const onTextFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatchGlobalSetting({ type: props.id as SettingInterface.SettingCategory, value: event.target.value });
+  };
+
+  return (
+    <Stack sx={{ margin: '0 0 12px 0' }}>
+      <Typography variant="subtitle2">{props.title}</Typography>
+      <TextField
+        sx={{ marginTop: '4px' }}
+        value={globalSetting[props.id] as number}
+        onChange={onTextFieldChange}
+        type="number"
+        size="small"
+      />
+    </Stack>
+  );
+}
