9469440074597fe79876eb956d66aacfeb74022b
bluewarn <n5lp97@gmail.com>
2023-08-07 08:51:37 +0900

    필요없는 코드 제거

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,14 +3,11 @@
 interface ImportMetaEnv {
     readonly VITE_BASE_URL: string
     readonly VITE_BASE_HOSTNAME: string
-    readonly VITE_BUILD_ENV: string
-
     readonly VITE_MAXNUMCHATS_DEFAULT: string
     readonly VITE_FIREFOX_RATE_EXT_LINK: string
     readonly VITE_CHROMIUM_RATE_EXT_LINK: string
     readonly VITE_DONATE_LINK: string
     readonly VITE_DOCUMENTATION: string
-    // more env variables...
 }
 
 interface ImportMeta {
