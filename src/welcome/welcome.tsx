2e37ac9807f7c8b5583d19ad929fdc006ded13e7
bluewarn <n5lp97@gmail.com>
2024-08-24 20:29:44 +0900

    feat: Sentry 제거

--- src/welcome/welcome.tsx
+++ src/welcome/welcome.tsx
@@ -16,21 +16,6 @@ import Link from "@mui/material/Link";
 import Alert from "@mui/material/Alert";
 import useExtensionGlobalSetting from "@hooks/useGlobalSettingExtension";
 
-import * as Sentry from "@sentry/browser";
-
-Sentry.init({
-  dsn: "https://af1b53df8897a90d7c27e8f9347954af@o1197585.ingest.sentry.io/4506447984852992",
-  integrations: [
-    new Sentry.Replay({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  release: browser.runtime.getManifest().version,
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
 
 function DocumentLink(props: { children: React.ReactNode }) {
     return (
