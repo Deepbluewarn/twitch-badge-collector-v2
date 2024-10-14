51ebd41b4acd15d4ef6a0e8c69c8e4b198428e4b
bluewarn <n5lp97@gmail.com>
2024-10-14 15:41:11 +0900

    유틸함수 추가

--- src/utils/utils-browser.ts
+++ src/utils/utils-browser.ts
@@ -7,3 +7,15 @@ export const isFirefoxAddon = async () => {
     }
     return false;
 }
+
+export const addStorageUpdateListener = (cb: (key: string, newValue: any) => void) => {
+    browser.storage.onChanged.addListener((changed, areaName) => {
+        if (areaName !== "local") return;
+        
+        for (const key in changed) {
+            const newValue = changed[key].newValue;
+            Logger('Storage Updated', `${key}: ${newValue}`);
+            cb(key, newValue);
+        }
+    });
+}
