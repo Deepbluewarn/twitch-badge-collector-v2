e922cde00722e98ed5678369c82f5cdc3b1d4090
bluewarn <n5lp97@gmail.com>
2024-10-14 15:39:44 +0900

    Logger 함수 추가

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,3 +1,5 @@
-export default function Logger(subject: string, message: string) {
-    console.log(`[TBC EXTENSION] ${subject} - ${message}`);
+function Logger(topic: string, message: string) {
+    const tbcStyle = 'color: #FFC107; font-weight: bold;';
+    const defaultStyle = 'color: inherit; font-weight: normal;';
+    console.debug(`%c★[TBC]%c ${topic} - ${message}`, tbcStyle, defaultStyle);
 }
